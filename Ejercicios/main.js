fef694f488d913b5c196b4ef38150cb8bc1eaa14
Nataliapedrazab <nataliapedraza756@gmail.com>
2024-09-26 18:53:21 -0500

    Subiendo los proyectos realizados de la clase, los proyectos son, Calculadora, Ejercicios, numeros pares y Suma de valores

---
+++ Ejercicios/main.js
@@ -0,0 +1,25 @@
+let listamercado = ["Pasta", "Papa criolla", "Helado", "Carne", "Pollo"]
+
+console.log(listamercado)
+console.log(listamercado.length)
+
+listamercado.push("costillas de cerdo")
+console.log(listamercado)
+// listamercado.forEach(item,index) =>{
+//    console.log(item + " " +index)
+// })
+
+console.log("conseguir un huevo")
+console.log("conseguir una sarten")
+console.log("conseguir aceite o mantequilla")
+variable = ""
+console.log("conseguir una espatula")
+console.log("colocar la sarten en la estufa")
+console.log("encender la estufa a fuego medio")
+if(variable == "aceite"){
+    console.log("añadir aceite a la sarten")
+} else if (variable == "mantequilla"){
+    console.log("agregar mantequilla a la sarten")
+}else {
+    console.log("apagar la estufa y buscar aceite o mantequilla")
+}
